77dacfd5ff0c5ed650ebda5e575f19a00322df97
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-09-02 02:43:01 +0300

    Create Test Using Page Object Model

--- src/main/java/com/saucedemo/pages/ProductsPage.java
+++ src/main/java/com/saucedemo/pages/ProductsPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 
 public class ProductsPage extends BasePage {
 
-    private By productsHeader = By.xpath("//span[text()='products']");
+    private By productsHeader = By.xpath("//span[text()='Products']");
 
     public boolean isProductsHeaderDisplayed() {
         return find(productsHeader).isDisplayed();
