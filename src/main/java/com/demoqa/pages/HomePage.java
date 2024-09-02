38e79412c9cc40741ee0203d08ea8e5af9432727
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-09-02 17:27:30 +0300

    Scroll To Element Using JavaScriptExecutor

--- src/main/java/com/demoqa/pages/HomePage.java
+++ src/main/java/com/demoqa/pages/HomePage.java
@@ -1,6 +1,16 @@
 package com.demoqa.pages;
 
-import com.saucedemo.pages.BasePage;
+import com.base.BasePage;
+import org.openqa.selenium.By;
+
+import static utilities.JavaScriptUtility.scrollToElementJS;
 
 public class HomePage extends BasePage {
+    private By formsCard = By.xpath("//h5[text()='Forms']");
+
+    public FormsPage goToForms() {
+        scrollToElementJS(formsCard);
+        click(formsCard);
+        return new FormsPage();
+    }
 }
--- src/test/java/part2/com/saucedemo/base/BaseTest.java
+++ src/test/java/part2/com/saucedemo/base/BaseTest.java
@@ -1,12 +1,11 @@
 package part2.com.saucedemo.base;
 
-import com.saucedemo.pages.BasePage;
+import com.base.BasePage;
 import com.saucedemo.pages.LoginPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
 
 public class BaseTest {
     protected WebDriver driver;
