545b18901512cd0615556aa49feaebdd979fa086
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-09-03 16:12:17 +0300

    Working With Links

--- src/main/java/com/demoqa/pages/elements/ElementsPage.java
+++ src/main/java/com/demoqa/pages/elements/ElementsPage.java
@@ -4,10 +4,16 @@ import com.demoqa.pages.HomePage;
 import org.openqa.selenium.By;
 
 public class ElementsPage extends HomePage {
-    private By webTablesMenuItem = By.xpath("//li[@id='item-3']//span[text()='Web Tables']");
+    private By webTablesMenuItem = By.xpath("//li[@id='item-3']/span[text()='Web Tables']");
+    private By LinksMenuItem = By.xpath("//li[@id='item-5']/span[text()='Links']");
 
     public WebTablesPage clickWebTables () {
         click(webTablesMenuItem);
         return new WebTablesPage();
+    }
+
+    public LinksPage clickLinks() {
+        click(LinksMenuItem);
+        return new LinksPage();
     }
 }
