a8d8a5b20f3fa01d85e082b7baf6055d99e228fb
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-09-03 13:16:32 +0300

    Working With Checkboxes

--- src/main/java/com/demoqa/pages/Forms/PracticeFormPage.java
+++ src/main/java/com/demoqa/pages/Forms/PracticeFormPage.java
@@ -6,6 +6,9 @@ import static utilities.JavaScriptUtility.*;
 
 public class PracticeFormPage extends FormsPage{
     private By femaleRadioButton =By.id("#gender-radio-2");
+    private By sportsHobbyCheckbox = By.id("#hobbies-checkbox-1");
+    private By readingHobbyCheckbox = By.id("#hobbies-checkbox-2");
+    private By musicHobbyCheckbox = By.id("#hobbies-checkbox-3");
 
     public void clickFemaleRadioButton() {
         scrollToElementJS(femaleRadioButton);
@@ -14,5 +17,37 @@ public class PracticeFormPage extends FormsPage{
 
     public boolean isFemaleRadioSelected() {
         return find(femaleRadioButton).isSelected();
+    }
+
+    public void clickSportsHobbyCheckbox() {
+        if(!find(sportsHobbyCheckbox).isSelected()) {
+            scrollToElementJS(sportsHobbyCheckbox);
+            clickJS(sportsHobbyCheckbox);
+        }
+    }
+
+    public void clickReadingHobbyCheckbox() {
+        if(!find(readingHobbyCheckbox).isSelected()) {
+            scrollToElementJS(readingHobbyCheckbox);
+            clickJS(readingHobbyCheckbox);
+        }
+    }
+
+    public void clickMusicHobbyCheckbox() {
+        if(!find(musicHobbyCheckbox).isSelected()) {
+            scrollToElementJS(musicHobbyCheckbox);
+            clickJS(musicHobbyCheckbox);
+        }
+    }
+
+    public void unclickReadingHobbyCheckbox() {
+        if(find(readingHobbyCheckbox).isSelected()) {
+            scrollToElementJS(readingHobbyCheckbox);
+            clickJS(readingHobbyCheckbox);
+        }
+    }
+
+    public boolean isReadingSelected() {
+        return find(readingHobbyCheckbox).isSelected();
     }
 }
---
+++ src/test/java/part3_4/com/demoqa/tests/part3/forms/CheckboxTest.java
@@ -0,0 +1,20 @@
+package part3_4.com.demoqa.tests.part3.forms;
+
+import com.demoqa.pages.Forms.FormsPage;
+import com.demoqa.pages.Forms.PracticeFormPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import part3_4.com.demoqa.base.BaseTest;
+
+public class CheckboxTest extends BaseTest {
+    @Test
+    public void testCheckbox() {
+        PracticeFormPage formsPage = homePage.goToForms().clickPracticeForm();
+        formsPage.clickSportsHobbyCheckbox();
+        formsPage.clickReadingHobbyCheckbox();
+        formsPage.clickMusicHobbyCheckbox();
+        formsPage.unclickReadingHobbyCheckbox();
+
+        Assert.assertFalse(formsPage.isReadingSelected(), "\n \n Reading Checkbox Is Selected \n");
+    }
+}
